1f5397e85e32f45a771dc06cc17c42c4bc4a8762
Bryan L Bottorff <bryanlbottorff@gmail.com>
2020-03-02 03:12:55 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,14 +1,9 @@
 # Your code goes here!
 class Anagram
    attr_accessor :word
-  # :diaper, :ba, :listen, :allergy
   
   def initialize(word)
     @word = word
-    # @diaper = diaper
-    # @ba = ba
-    # @listen = listen
-    # @allergy = allergy
   end
   
   def match(words_a)
